f593224862343cfc32f4fbadb67fc4437ebf4cba
rabindranath-create <87770453+rabindranath-create@users.noreply.github.com>
2025-07-09 18:43:19 -0500

    Update main_script253.R

--- main_script253.R
+++ main_script253.R
@@ -46,8 +46,8 @@ for (i in 1:100) {
   obs_gen_para <- c(gamma = 0.3, d = 5, noPoints = 100, no_c = 25, no_o = 75)
   result <- MACS_Alg_M(obs_gen_para, kappa = j, lambda, cost)
   
-  results_3[4*(i-1) + j + 1, ] <- list(
-    Run = 4*(i-1) + j + 1,
+  results_3[4*(i-1) + j/2, ] <- list(
+    Run = 4*(i-1) + j/2,
     N_t = 75,
     K = j,
     Length = result$Length_total,
